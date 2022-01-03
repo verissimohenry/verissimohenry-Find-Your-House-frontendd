dbc92c25bc6a9cf14346fb8e19baeeae7b0b65b9
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-03 10:40:55 +0100

    Add assest folder

---
+++ src/api/railshouse.js
@@ -0,0 +1,5 @@
+export const API_ID = 'https://tranquil-shore-53426.herokuapp.com/';
+export const API_LOGIN = 'sessions';
+export const API_REGISTER = 'registrations';
+export const API_HOUSE = 'houses';
+export const API_FAVOURITE = 'favourites';
