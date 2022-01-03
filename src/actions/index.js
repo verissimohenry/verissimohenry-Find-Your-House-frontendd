3f2f3dfa9269cc5987cac163127b55b024dc992a
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-03 13:08:26 +0100

    modify package.json

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,26 +5,26 @@ export const ERROR = 'ERROR';
 export const FAV = 'FAV';
 
 export const getHouseAction = data => ({
-    type: HOUSE,
-    payload: data,
+  type: HOUSE,
+  payload: data,
 });
 
 export const getHouseDetail = detail => ({
-    type: HOUSEDETAIL,
-    payload: detail,
+  type: HOUSEDETAIL,
+  payload: detail,
 });
 
 export const getUserToken = token => ({
-    type: USERTOKEN,
-    payload: token,
+  type: USERTOKEN,
+  payload: token,
 });
 
 export const getError = error => ({
-    type: ERROR,
-    payload: error,
+  type: ERROR,
+  payload: error,
 });
 
 export const getFavAction = data => ({
-    type: FAV,
-    payload: data,
+  type: FAV,
+  payload: data,
 });
