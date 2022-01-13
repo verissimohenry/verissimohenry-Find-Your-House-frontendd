ca8b6d1dc9c9bc59b7e1d3382974344fcea5c6ba
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-13 16:45:31 +0100

    modify app

--- src/api/railshouse.js
+++ src/api/railshouse.js
@@ -1,12 +1,12 @@
 let url = '';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'https://tranquil-tundra-99565.herokuapp.com/';
+    url = 'https://polar-escarpment-09730.herokuapp.com/';
 } else {
     url = 'http://localhost:3001/';
 }
 
-export const API_ID = 'https://tranquil-tundra-99565.herokuapp.com/';
+export const API_ID = 'https://polar-escarpment-09730.herokuapp.com/';
 export const API_LOGIN = 'sessions';
 export const API_REGISTER = 'registrations';
 export const API_HOUSE = 'houses';
