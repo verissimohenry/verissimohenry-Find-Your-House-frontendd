4747a15971394e141cf59fe667b188103ca8b4ba
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-13 14:56:15 +0100

    modify app

--- src/api/railshouse.js
+++ src/api/railshouse.js
@@ -1,4 +1,12 @@
-export const API_ID = 'https://tranquil-shore-53426.herokuapp.com/';
+let url = '';
+
+if (process.env.NODE_ENV === 'production') {
+    url = 'https://tranquil-tundra-99565.herokuapp.com/';
+} else {
+    url = 'http://localhost:3001/';
+}
+
+export const API_ID = 'https://tranquil-tundra-99565.herokuapp.com/';
 export const API_LOGIN = 'sessions';
 export const API_REGISTER = 'registrations';
 export const API_HOUSE = 'houses';
