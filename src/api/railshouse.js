ea32401c67fd5c2db0a46940085c3f18834174ab
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-14 12:26:33 +0100

    modify app

--- src/api/railshouse.js
+++ src/api/railshouse.js
@@ -1,12 +1,12 @@
 let url = '';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'https://polar-escarpment-09730.herokuapp.com/';
+  url = 'https://tranquil-shore-53426.herokuapp.com/';
 } else {
-    url = 'http://localhost:3001/';
+  url = 'http://localhost:3001/';
 }
 
-export const API_ID = 'https://polar-escarpment-09730.herokuapp.com/';
+export const API_ID = url;
 export const API_LOGIN = 'sessions';
 export const API_REGISTER = 'registrations';
 export const API_HOUSE = 'houses';
