f7eb217ae4359b50a87e55cf4cf4a74916b2ee8b
verissimohenry <verissimohenry04@gmail.com>
2022-05-20 12:13:42 +0100

    Modify test folders

--- src/__tests__/components/error.test.js
+++ src/__tests__/components/error.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import error from '../../components/Error';
 
-it('renders error component correctly', () => {
+it('renders correctly', () => {
   const error = renderer.create(<error />).toJSON();
   expect(error).toMatchSnapshot();
 });
