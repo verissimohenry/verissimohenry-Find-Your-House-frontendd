63a63b3bc14a33ca476697e47428fe43e8aea25b
verissimohenry <verissimohenry04@gmail.com>
2022-05-21 15:39:17 +0100

    fix linter error in housecarddom file

--- src/__tests__/housecarddom.test.js
+++ src/__tests__/housecarddom.test.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { BrowserRouter } from 'react-router-dom';
-import HouseCard from '../components/HouseCard';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter } from "react-router-dom";
+import HouseCard from "../components/HouseCard";
 
 afterEach(cleanup);
 
-  it('render name', () => {
-    const house = {
-      id: 1,
-      name: 'one family down',
-      price: 9000,
-      photo: 'randompic',
-      description: 'This is a house description',
-    };
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <HouseCard key={house.id} house={house} />
-      </BrowserRouter>
-    );
-    expect(getByTestId('1')).toHaveTextContent('one family down');
-  });
+it("render name", () => {
+  const house = {
+    id: 1,
+    name: "one family down",
+    price: 9000,
+    photo: "randompic",
+    description: "This is a house description",
+  };
+  const { getByTestId } = render(
+    <BrowserRouter>
+      <HouseCard key={house.id} house={house} />{" "}
+    </BrowserRouter>
+  );
+  expect(getByTestId("1")).toHaveTextContent("one family down");
+});
 
-  it('render price', () => {
-    const house = {
-      id: 1,
-      name: 'one family down',
-      price: 9000,
-      photo: 'randompic',
-      description: 'This is a house description',
-    };
-    const { getByTestId } = render(
-      <BrowserRouter>
-        <HouseCard key={house.id} house={house} />
-      </BrowserRouter>
-    );
-    expect(getByTestId('1')).toHaveTextContent('9000');
-  });
-
+it("render price", () => {
+  const house = {
+    id: 1,
+    name: "one family down",
+    price: 9000,
+    photo: "randompic",
+    description: "This is a house description",
+  };
+  const { getByTestId } = render(
+    <BrowserRouter>
+      <HouseCard key={house.id} house={house} />{" "}
+    </BrowserRouter>
+  );
+  expect(getByTestId("1")).toHaveTextContent("9000");
+});
